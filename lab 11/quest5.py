ab47ea1fe805545e349a95072040f41cb0fed80b
Goerci <ifmggoerci4g@gmail.com>
2026-06-16 15:34:57 -0300

    a

---
+++ lab 11/quest5.py
@@ -0,0 +1,24 @@
+import random
+num1 = random.randint(1,10)
+num2 = random.randint(1,10)
+num3 = random.randint(1,10)
+life = 10
+cont = 0
+while life > 0:
+    life -= 1
+    cont += 1
+    print(f"==== Chance {cont} ====")
+    palp1 = int(input("Insira um palpite do Primeiro numero"))
+    palp2 = int(input("Insira um palpite do Segundo numero"))
+    palp3 = int(input("Insira um palpite do Terceiro numero"))
+    print(f"Palpite: {palp1} {palp2} {palp3}")
+    if palp1 or palp 3
+        palp1 = !
+    if palp2 == num2
+        palp2 == !
+    if palp3 == num3
+        palp3 == !
+        
+    
+    
+    
